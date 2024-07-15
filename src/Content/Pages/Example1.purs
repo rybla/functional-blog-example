9bee8037896e27d1158a0b8262f4d6dbaea20671
Henry Blanchette <henry@monkeyface.com>
2024-07-14 22:15:07 -0400

    formatting

--- src/Content/Pages/Example1.purs
+++ src/Content/Pages/Example1.purs
@@ -1,16 +1,10 @@
 --props { "public_path": "example1" }
 module Content.Pages.Example1 where
 
-import Prelude
-
 import Content (mkSomeContent)
 import Content.Notes.NoteA (NoteA)
-import Halogen (defaultEval, mkComponent, mkEval)
-import Halogen.HTML as HH
 import Page as Page
 import Type.Proxy (Proxy(..))
-
--- =============================================================================
 
 static_content = Page.static_content spec
 start_client = Page.start_client spec
@@ -21,5 +15,9 @@ spec :: Page.Spec
 spec = Page.Spec
   { title: "Example1"
   , static_content: "This is a placeholder."
-  , content: mkSomeContent (Proxy :: Proxy NoteA)
+  , content: mkSomeContent
+      ( Proxy
+          :: Proxy
+               NoteA
+      )
   }
--- src/Content/Pages/Example2.purs
+++ src/Content/Pages/Example2.purs
@@ -1,18 +1,11 @@
 --props { "public_path": "example2" }
 module Content.Pages.Example2 where
 
-import Prelude
-
-import Content
+import Content (type (:), Column, Nil, mkSomeContent)
 import Content.Notes.NoteA (NoteA)
 import Content.Notes.NoteB (NoteB)
-import Data.Identity (Identity)
-import Halogen (defaultEval, mkComponent, mkEval)
-import Halogen.HTML as HH
 import Page as Page
 import Type.Proxy (Proxy(..))
-
--- =============================================================================
 
 static_content = Page.static_content spec
 start_client = Page.start_client spec
@@ -23,7 +16,15 @@ spec :: Page.Spec
 spec = Page.Spec
   { title: "Example2"
   , static_content: "This is a placeholder for Example2."
-  , content: mkSomeContent (Proxy :: Proxy (Column (NoteA : NoteB : Nil)))
+  , content: mkSomeContent
+      ( Proxy
+          :: Proxy
+               ( Column
+                   ( NoteA
+                       : NoteB
+                       : Nil
+                   )
+               )
+      )
   }
-
 
